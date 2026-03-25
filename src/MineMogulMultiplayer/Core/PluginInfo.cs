f1955150a1e6e840186b71e07480bf637ede535e
BLUNT-Chief <BLUNT-Chief@users.noreply.github.com>
2026-03-25 03:35:35 -0500

    v1.4.22: Version bump to test Nexus auto-publish

--- src/MineMogulMultiplayer/Core/PluginInfo.cs
+++ src/MineMogulMultiplayer/Core/PluginInfo.cs
@@ -7,6 +7,6 @@ namespace MineMogulMultiplayer.Core
     {
         public const string GUID = "com.minemogul.multiplayer";
         public const string Name = "MineMogul Multiplayer";
-        public const string Version = "1.4.21";
+        public const string Version = "1.4.22";
     }
 }
